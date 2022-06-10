b493e5ffa7e774528fec59d0d51c0303972bb12f
tametadada <nanuna2009@gmail.com>
2022-06-10 19:06:04 +0000

    Adding temporary changes to my git

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["tametaapp.us-south.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["tamapp.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
