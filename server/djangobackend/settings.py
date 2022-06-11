f708422870a727903f0042d98e207c52b24091e6
tametadada <nanuna2009@gmail.com>
2022-06-11 16:06:26 +0000

    after initial deployment

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["tamapp.us-south.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["mycardealapp.eu-gb.cf.appdomain.cloud", "localhost", "*"]
 
 
 # Application definition
