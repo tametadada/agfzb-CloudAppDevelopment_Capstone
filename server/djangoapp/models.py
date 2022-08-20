13c23b8f07493f8d2b7bb3186e04c27689bd03b1
tametadada <nanuna2009@gmail.com>
2022-08-20 15:32:41 +0000

    Adding proxies & fixing bugs

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -55,3 +55,36 @@ class CarModel(models.Model):
         return 'Name ' + self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip, state):
+        self.address = address
+        self.city = city
+        self.full_name = full_name
+        self.id = id
+        self.lat = lat
+        self.long = long
+        self.short_name = short_name
+        self.st = st
+        self.zip = zip
+        self.state = state
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
+# <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+        self.id = id
+
+    def __str__(self):
+        return "Review name: " + self.name
+
