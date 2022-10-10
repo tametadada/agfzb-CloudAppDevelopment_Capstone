2ee7b29b7852a1b7ce3ac61ae05f367506fb2a2d
tametadada <nanuna2009@gmail.com>
2022-10-10 15:15:29 -0400

    updates on Dockerfile & deployment.yaml

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["mycardealapp.us-south.cf.appdomain.cloud", "localhost", "*", "https://nanuna2009-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+ALLOWED_HOSTS = ["mycardealapp.us-south.cf.appdomain.cloud", "localhost", 'localhost:8000', "https://nanuna2009-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
 
 # Application definition
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
+
+# CSRF_TRUSTED_ORIGINS = ['https://ksundararaja-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
